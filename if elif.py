00445d893a15a3bee910704dfdb81960b20d8f73
kiranariga <kiranariga22@gmail.com>
2022-09-22 19:00:08 +0530

    22/09/22

---
+++ if elif.py
@@ -0,0 +1,9 @@
+a = int(input("enter first value:"))
+b = int(input("enter second value: "))
+c = int(input("enter third value: "))
+if a>b and a>c:
+    print("first value greatest")
+elif b>a and b>c:
+    print("second value greatest")
+elif c>a and c>b:
+    print("third value greatest")        
